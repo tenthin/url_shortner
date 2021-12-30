feef53d6404dec57819c7077afd8d5c54fb653ec
Tenzin <tenthinten@gmail.com>
2021-12-30 20:09:19 +0530

    adding url and generating shortened url

--- shortner/admin.py
+++ shortner/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Url      #After creating model and migration in models.py, import in admin.py 
 # Register your models here.
+admin.site.register(Url)
